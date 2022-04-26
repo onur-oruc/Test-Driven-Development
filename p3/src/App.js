200a3734afb9195b9c12e72bccfd755906825714
onur-oruc <onurorucbilkent@gmail.com>
2022-04-26 16:14:17 +0300

    Add country information

--- p3/src/App.js
+++ p3/src/App.js
@@ -7,7 +7,7 @@ import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Geocode from 'react-geocode';
 var sunCalc = require('suncalc');
-
+const geolib = require('geolib');
 var API_key='AIzaSyD-_iRPdJV5WRTyf2EDbyc-vfbuFTr05W4';
 
 // https://www.npmjs.com/package/react-geocode
@@ -20,24 +20,72 @@ function App() {
   const [isCorrectFormat, setIsCorrectFormat] = useState(true);
   const [isMissingFields, setIsMissingFields] = useState(true);
   const [formattedAddress, setFormattedAddress] = useState('');
- 
+  const [country, setCountry] = useState(null);
+  const [distanceToNorthPole, setDistanceToNorthPole] = useState(null);
+  const [distanceToMoon, setDistanceToMoon] = useState(null);
+
   Geocode.setApiKey(API_key);
   Geocode.setLocationType("ROOFTOP");
   Geocode.enableDebug();
 
-  let pattern = new RegExp('^-?([1-8]?[1-9]|[1-9]0)\\.{1}\\d{1,6}'); // fix this!!!
+  let pattern = new RegExp('^-?([1-8]?[0-9]|[1-9]0)\\.{1}\\d{1,6}'); // fix this!!!
+  function isLatitude(lat) {
+    return isFinite(lat) && Math.abs(lat) <= 90;
+  }
+
+  function isLongitude(lng) {
+    return isFinite(lng) && Math.abs(lng) <= 180;
+  }
+
   const findLocationBtn = () => {
     if (!isMissingFields && isCorrectFormat) {
       console.log("lat: " + typeof(latitude) + " lat: " + latitude);
       console.log("lng: " + typeof(longitude) + " lng: " + longitude);
       Geocode.fromLatLng(latitude, longitude).then(
         (response) => {
+          console.dir(response);
           const address = response.results[0].formatted_address;
           setFormattedAddress(address);
           console.log("address: ", address);
+          var add = response.results[0].formatted_address;
+          var value = add.split(",");
+          var count = value.length;
+          setCountry(value[count-1]);
         }
       )
     }
+  }
+
+  const calculateDistanceToNorthPole = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+        const _distanceToNorthPole = geolib.getDistance(
+          {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }, 
+          {
+            latitude: 90.0000000,
+            longitude: 0.0000000,
+          }
+        );
+        Geocode.fromLatLng(position.coords.latitude, position.coords.longitude).then(
+          (response) => {
+            const address = response.results[0].formatted_address;
+            setFormattedAddress(address);
+            console.log("address: ", address);
+            var add = response.results[0].formatted_address;
+            var value = add.split(",");
+            var count = value.length;
+            setCountry(value[count-1]);
+          }
+        )
+        console.log("distance to north pole: ", _distanceToNorthPole);
+        setDistanceToNorthPole(_distanceToNorthPole/1000);
+        setDistanceToMoon(null);
+      });
   }
 
   const getDistanteMoonAuto = () => {
@@ -55,11 +103,22 @@ function App() {
             const address = response.results[0].formatted_address;
             setFormattedAddress(address);
             console.log("address: ", address);
+            var add = response.results[0].formatted_address;
+            var value = add.split(",");
+            var count = value.length;
+            setCountry(value[count-1]);
           }
         )
+        setDistanceToMoon(Math.round(sunCalc.getMoonPosition(new Date(), position.coords.latitude, position.coords.longitude)['distance']));
       }, () => {
         setStatus('Unable to retrieve your location');
       });
+    }
+  }
+
+  const getDistanceMoonCustom = () => {
+    if (!isMissingFields && isCorrectFormat) {
+      setDistanceToMoon(Math.round(sunCalc.getMoonPosition(new Date(), latitude, longitude)['distance']));
     }
   }
 
@@ -71,7 +130,7 @@ function App() {
     else {
       setIsMissingFields(false);
 
-      if (pattern.test(longitude) && pattern.test(latitude)) {
+      if (isLatitude(latitude) && isLongitude(longitude)) {
         // api call
         setIsCorrectFormat(true);
 
@@ -119,12 +178,14 @@ function App() {
           
         <div className='App__DistanceButtons'>
           <button
-            id="north-pole-distance-btn">
+            id="north-pole-distance-btn"
+            onClick={calculateDistanceToNorthPole}>
               Distance to North Pole
           </button>
 
           <button
-            id="calc-distance-to-moon-manual">
+            id="calc-distance-to-moon-manual"
+            onClick={getDistanceMoonCustom}>
               Distance to Moon
           </button>
 
@@ -150,21 +211,30 @@ function App() {
               color='primary'
               >Location:&nbsp;&nbsp;</FormLabel>
             <FormLabel 
-              id="city"
-              color='primary'
+                id="city"
+                color='primary'
               >{formattedAddress}</FormLabel>
             {/* <InputLabel>Location:&nbsp;&nbsp;</InputLabel>
             <InputLabel id="city">{formattedAddress}</InputLabel> */}
           </div>
           <div className='App_InformationLabels'>
+            <FormLabel
+              color='primary'
+              >Country:&nbsp;&nbsp;</FormLabel>
+            <FormLabel 
+              id="country"
+              color='primary'
+            >{country}</FormLabel>
+          </div>
+          <div className='App_InformationLabels'>
             <InputLabel>Distance to North Pole:&nbsp;&nbsp;</InputLabel>
-            <InputLabel id="north-pole-distance">3</InputLabel>
-            <InputLabel>&nbsp;miles</InputLabel>
+            <InputLabel id="north-pole-distance">{distanceToNorthPole}</InputLabel>
+            <InputLabel>&nbsp;km</InputLabel>
           </div>
           <div className='App_InformationLabels'>
             <InputLabel>Distance to Moon:&nbsp;&nbsp;</InputLabel>
-            <InputLabel id="distance-to-moon">33</InputLabel>
-            <InputLabel>&nbsp;miles</InputLabel>
+            <InputLabel id="distance-to-moon">{distanceToMoon}</InputLabel>
+            <InputLabel>&nbsp;km</InputLabel>
           </div>
         </div>
     </div>
